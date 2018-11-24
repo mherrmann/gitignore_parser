e50b79f2ea593526b0e031183776694300cd08e4
Michael Herrmann <michael@herrmann.io>
2018-11-24 11:27:43 +0100

    Add setup.py for uploading to PyPI

---
+++ setup.py
@@ -0,0 +1,43 @@
+"""A spec-compliant gitignore parser for Python 3.5+
+
+See:
+https://github.com/mherrmann/gitignore_parser
+"""
+
+from glob import glob
+from os.path import relpath, join, isdir
+
+from setuptools import setup, find_packages
+
+description = 'A spec-compliant gitignore parser for Python 3.5+'
+setup(
+    name='gitignore_parser',
+    version='0.0.1',
+    description=description,
+    long_description=
+        description + '\n\nhttps://github.com/mherrmann/gitignore_parser',
+    author='Michael Herrmann',
+    author_email='michael+removethisifyouarehuman@herrmann.io',
+    url='https://github.com/mherrmann/gitignore_parser',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+    
+        'License :: OSI Approved :: MIT License',
+    
+        'Operating System :: OS Independent',
+    
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+    license='MIT',
+    keywords='gitignore',
+    platforms=['MacOS', 'Windows', 'Debian', 'Fedora', 'CentOS']
+)
