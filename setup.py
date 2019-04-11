17dd0ca549b37289f803beb48f3bc2a9f6fab075
Deepcommit <48901781+deepcommit@users.noreply.github.com>
2019-04-10 22:23:38 -0700

    Fixed PYTHON_W293 issues in setup.py

--- setup.py
+++ setup.py
@@ -20,11 +20,11 @@ setup(
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
-    
+
         'License :: OSI Approved :: MIT License',
-    
+
         'Operating System :: OS Independent',
-    
+
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
