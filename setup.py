912cc663b535476725db937515e9d25d2fc227d6
Jean-Baptiste VESLIN <jean-baptiste.veslin@meteo.fr>
2020-02-26 12:06:11 +0100

    Add pathlib as required (for python2 usage)
    Even if designed for python3, gitignore_parser perfectly works with python2, except that pathlib is not provided by default with python2.

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ description = 'A spec-compliant gitignore parser for Python 3.5+'
 setup(
     name='gitignore_parser',
     version='0.0.5',
+    install_requires=['pathlib'],
     description=description,
     long_description=
         description + '\n\nhttps://github.com/mherrmann/gitignore_parser',
