1e68ceb6c2c7bb246e469cbe4290bd557cdec288
Michael Herrmann <michael@herrmann.io>
2020-02-28 07:05:20 +0100

    Bump version for release 0.0.6

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 description = 'A spec-compliant gitignore parser for Python 3.5+'
 setup(
     name='gitignore_parser',
-    version='0.0.5',
+    version='0.0.6',
     install_requires=["pathlib; python_version < '3.0'"],
     description=description,
     long_description=
