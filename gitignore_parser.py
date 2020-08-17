afce73dcddce182c9aeb44e01804e172ffc4c6ee
Michael Herrmann <michael@herrmann.io>
2020-08-17 07:20:55 +0200

    Replace tabs by spaces

--- gitignore_parser.py
+++ gitignore_parser.py
@@ -6,201 +6,201 @@ from os.path import dirname
 from pathlib import Path
 
 def handle_negation(file_path, rules):
-	matched = False
-	for rule in rules:
-		if rule.match(file_path):
-			if rule.negation:
-				matched = False
-			else:
-				matched = True
-	return matched
+    matched = False
+    for rule in rules:
+        if rule.match(file_path):
+            if rule.negation:
+                matched = False
+            else:
+                matched = True
+    return matched
 
 def parse_gitignore(full_path, base_dir=None):
-	if base_dir is None:
-		base_dir = dirname(full_path)
-	rules = []
-	with open(full_path) as ignore_file:
-		counter = 0
-		for line in ignore_file:
-			counter += 1
-			line = line.rstrip('\n')
-			rule = rule_from_pattern(line, base_path=Path(base_dir).resolve(),
-									 source=(full_path, counter))
-			if rule:
-				rules.append(rule)
-	if not any(r.negation for r in rules):
-		return lambda file_path: any(r.match(file_path) for r in rules)
-	else:
-		# We have negation rules. We can't use a simple "any" to evaluate them.
-		# Later rules override earlier rules.
-		return lambda file_path: handle_negation(file_path, rules)
+    if base_dir is None:
+        base_dir = dirname(full_path)
+    rules = []
+    with open(full_path) as ignore_file:
+        counter = 0
+        for line in ignore_file:
+            counter += 1
+            line = line.rstrip('\n')
+            rule = rule_from_pattern(line, base_path=Path(base_dir).resolve(),
+                                     source=(full_path, counter))
+            if rule:
+                rules.append(rule)
+    if not any(r.negation for r in rules):
+        return lambda file_path: any(r.match(file_path) for r in rules)
+    else:
+        # We have negation rules. We can't use a simple "any" to evaluate them.
+        # Later rules override earlier rules.
+        return lambda file_path: handle_negation(file_path, rules)
 
 def rule_from_pattern(pattern, base_path=None, source=None):
-	"""
-	Take a .gitignore match pattern, such as "*.py[cod]" or "**/*.bak",
-	and return an IgnoreRule suitable for matching against files and
-	directories. Patterns which do not match files, such as comments
-	and blank lines, will return None.
-	Because git allows for nested .gitignore files, a base_path value
-	is required for correct behavior. The base path should be absolute.
-	"""
-	if base_path and base_path != Path(base_path).resolve():
-		raise ValueError('base_path must be absolute')
-	# Store the exact pattern for our repr and string functions
-	orig_pattern = pattern
-	# Early returns follow
-	# Discard comments and separators
-	if pattern.strip() == '' or pattern[0] == '#':
-		return
-	# Discard anything with more than two consecutive asterisks
-	if pattern.find('***') > -1:
-		return
-	# Strip leading bang before examining double asterisks
-	if pattern[0] == '!':
-		negation = True
-		pattern = pattern[1:]
-	else:
-		negation = False
-	# Discard anything with invalid double-asterisks -- they can appear
-	# at the start or the end, or be surrounded by slashes
-	for m in re.finditer(r'\*\*', pattern):
-		start_index = m.start()
-		if (start_index != 0 and start_index != len(pattern) - 2 and
-				(pattern[start_index - 1] != '/' or
-				 pattern[start_index + 2] != '/')):
-			return
+    """
+    Take a .gitignore match pattern, such as "*.py[cod]" or "**/*.bak",
+    and return an IgnoreRule suitable for matching against files and
+    directories. Patterns which do not match files, such as comments
+    and blank lines, will return None.
+    Because git allows for nested .gitignore files, a base_path value
+    is required for correct behavior. The base path should be absolute.
+    """
+    if base_path and base_path != Path(base_path).resolve():
+        raise ValueError('base_path must be absolute')
+    # Store the exact pattern for our repr and string functions
+    orig_pattern = pattern
+    # Early returns follow
+    # Discard comments and separators
+    if pattern.strip() == '' or pattern[0] == '#':
+        return
+    # Discard anything with more than two consecutive asterisks
+    if pattern.find('***') > -1:
+        return
+    # Strip leading bang before examining double asterisks
+    if pattern[0] == '!':
+        negation = True
+        pattern = pattern[1:]
+    else:
+        negation = False
+    # Discard anything with invalid double-asterisks -- they can appear
+    # at the start or the end, or be surrounded by slashes
+    for m in re.finditer(r'\*\*', pattern):
+        start_index = m.start()
+        if (start_index != 0 and start_index != len(pattern) - 2 and
+                (pattern[start_index - 1] != '/' or
+                 pattern[start_index + 2] != '/')):
+            return
 
-	# Special-casing '/', which doesn't match any files or directories
-	if pattern.rstrip() == '/':
-		return
+    # Special-casing '/', which doesn't match any files or directories
+    if pattern.rstrip() == '/':
+        return
 
-	directory_only = pattern[-1] == '/'
-	# A slash is a sign that we're tied to the base_path of our rule
-	# set.
-	anchored = '/' in pattern[:-1]
-	if pattern[0] == '/':
-		pattern = pattern[1:]
-	if pattern[0] == '*' and len(pattern) >= 2 and pattern[1] == '*':
-		pattern = pattern[2:]
-		anchored = False
-	if pattern[0] == '/':
-		pattern = pattern[1:]
-	if pattern[-1] == '/':
-		pattern = pattern[:-1]
-	# patterns with leading hashes are escaped with a backslash in front, unescape it
-	if pattern[0] == '\\' and pattern[1] == '#':
-		pattern = pattern[1:]
-	# trailing spaces are ignored unless they are escaped with a backslash
-	i = len(pattern)-1
-	striptrailingspaces = True
-	while i > 1 and pattern[i] == ' ':
-		if pattern[i-1] == '\\':
-			pattern = pattern[:i-1] + pattern[i:]
-			i = i - 1
-			striptrailingspaces = False
-		else:
-			if striptrailingspaces:
-				pattern = pattern[:i]
-		i = i - 1
-	regex = fnmatch_pathname_to_regex(pattern, directory_only)
-	if anchored:
-		regex = ''.join(['^', regex])
-	return IgnoreRule(
-		pattern=orig_pattern,
-		regex=regex,
-		negation=negation,
-		directory_only=directory_only,
-		anchored=anchored,
-		base_path=Path(base_path) if base_path else None,
-		source=source
-	)
+    directory_only = pattern[-1] == '/'
+    # A slash is a sign that we're tied to the base_path of our rule
+    # set.
+    anchored = '/' in pattern[:-1]
+    if pattern[0] == '/':
+        pattern = pattern[1:]
+    if pattern[0] == '*' and len(pattern) >= 2 and pattern[1] == '*':
+        pattern = pattern[2:]
+        anchored = False
+    if pattern[0] == '/':
+        pattern = pattern[1:]
+    if pattern[-1] == '/':
+        pattern = pattern[:-1]
+    # patterns with leading hashes are escaped with a backslash in front, unescape it
+    if pattern[0] == '\\' and pattern[1] == '#':
+        pattern = pattern[1:]
+    # trailing spaces are ignored unless they are escaped with a backslash
+    i = len(pattern)-1
+    striptrailingspaces = True
+    while i > 1 and pattern[i] == ' ':
+        if pattern[i-1] == '\\':
+            pattern = pattern[:i-1] + pattern[i:]
+            i = i - 1
+            striptrailingspaces = False
+        else:
+            if striptrailingspaces:
+                pattern = pattern[:i]
+        i = i - 1
+    regex = fnmatch_pathname_to_regex(pattern, directory_only)
+    if anchored:
+        regex = ''.join(['^', regex])
+    return IgnoreRule(
+        pattern=orig_pattern,
+        regex=regex,
+        negation=negation,
+        directory_only=directory_only,
+        anchored=anchored,
+        base_path=Path(base_path) if base_path else None,
+        source=source
+    )
 
 whitespace_re = re.compile(r'(\\ )+$')
 
 IGNORE_RULE_FIELDS = [
-	'pattern', 'regex',  # Basic values
-	'negation', 'directory_only', 'anchored',  # Behavior flags
-	'base_path',  # Meaningful for gitignore-style behavior
-	'source'  # (file, line) tuple for reporting
+    'pattern', 'regex',  # Basic values
+    'negation', 'directory_only', 'anchored',  # Behavior flags
+    'base_path',  # Meaningful for gitignore-style behavior
+    'source'  # (file, line) tuple for reporting
 ]
 
 
 class IgnoreRule(collections.namedtuple('IgnoreRule_', IGNORE_RULE_FIELDS)):
-	def __str__(self):
-		return self.pattern
+    def __str__(self):
+        return self.pattern
 
-	def __repr__(self):
-		return ''.join(['IgnoreRule(\'', self.pattern, '\')'])
+    def __repr__(self):
+        return ''.join(['IgnoreRule(\'', self.pattern, '\')'])
 
-	def match(self, abs_path):
-		matched = False
-		if self.base_path:
-			rel_path = str(Path(abs_path).resolve().relative_to(self.base_path))
-		else:
-			rel_path = str(Path(abs_path))
-		if rel_path.startswith('./'):
-			rel_path = rel_path[2:]
-		if re.search(self.regex, rel_path):
-			matched = True
-		return matched
+    def match(self, abs_path):
+        matched = False
+        if self.base_path:
+            rel_path = str(Path(abs_path).resolve().relative_to(self.base_path))
+        else:
+            rel_path = str(Path(abs_path))
+        if rel_path.startswith('./'):
+            rel_path = rel_path[2:]
+        if re.search(self.regex, rel_path):
+            matched = True
+        return matched
 
 
 # Frustratingly, python's fnmatch doesn't provide the FNM_PATHNAME
 # option that .gitignore's behavior depends on.
 def fnmatch_pathname_to_regex(pattern, directory_only: bool):
-	"""
-	Implements fnmatch style-behavior, as though with FNM_PATHNAME flagged;
-	the path separator will not match shell-style '*' and '.' wildcards.
-	"""
-	i, n = 0, len(pattern)
-	
-	seps = [re.escape(os.sep)]
-	if os.altsep is not None:
-		seps.append(re.escape(os.altsep))
-	seps_group = '[' + '|'.join(seps) + ']'
-	nonsep = r'[^{}]'.format('|'.join(seps))
+    """
+    Implements fnmatch style-behavior, as though with FNM_PATHNAME flagged;
+    the path separator will not match shell-style '*' and '.' wildcards.
+    """
+    i, n = 0, len(pattern)
+    
+    seps = [re.escape(os.sep)]
+    if os.altsep is not None:
+        seps.append(re.escape(os.altsep))
+    seps_group = '[' + '|'.join(seps) + ']'
+    nonsep = r'[^{}]'.format('|'.join(seps))
 
-	res = []
-	while i < n:
-		c = pattern[i]
-		i += 1
-		if c == '*':
-			try:
-				if pattern[i] == '*':
-					i += 1
-					res.append('.*')
-					if pattern[i] == '/':
-						i += 1
-						res.append(''.join([seps_group, '?']))
-				else:
-					res.append(''.join([nonsep, '*']))
-			except IndexError:
-				res.append(''.join([nonsep, '*']))
-		elif c == '?':
-			res.append(nonsep)
-		elif c == '/':
-			res.append(seps_group)
-		elif c == '[':
-			j = i
-			if j < n and pattern[j] == '!':
-				j += 1
-			if j < n and pattern[j] == ']':
-				j += 1
-			while j < n and pattern[j] != ']':
-				j += 1
-			if j >= n:
-				res.append('\\[')
-			else:
-				stuff = pattern[i:j].replace('\\', '\\\\')
-				i = j + 1
-				if stuff[0] == '!':
-					stuff = ''.join(['^', stuff[1:]])
-				elif stuff[0] == '^':
-					stuff = ''.join('\\' + stuff)
-				res.append('[{}]'.format(stuff))
-		else:
-			res.append(re.escape(c))
-	res.insert(0, '(?ms)')
-	if not directory_only:
-		res.append('$')
-	return ''.join(res)
+    res = []
+    while i < n:
+        c = pattern[i]
+        i += 1
+        if c == '*':
+            try:
+                if pattern[i] == '*':
+                    i += 1
+                    res.append('.*')
+                    if pattern[i] == '/':
+                        i += 1
+                        res.append(''.join([seps_group, '?']))
+                else:
+                    res.append(''.join([nonsep, '*']))
+            except IndexError:
+                res.append(''.join([nonsep, '*']))
+        elif c == '?':
+            res.append(nonsep)
+        elif c == '/':
+            res.append(seps_group)
+        elif c == '[':
+            j = i
+            if j < n and pattern[j] == '!':
+                j += 1
+            if j < n and pattern[j] == ']':
+                j += 1
+            while j < n and pattern[j] != ']':
+                j += 1
+            if j >= n:
+                res.append('\\[')
+            else:
+                stuff = pattern[i:j].replace('\\', '\\\\')
+                i = j + 1
+                if stuff[0] == '!':
+                    stuff = ''.join(['^', stuff[1:]])
+                elif stuff[0] == '^':
+                    stuff = ''.join('\\' + stuff)
+                res.append('[{}]'.format(stuff))
+        else:
+            res.append(re.escape(c))
+    res.insert(0, '(?ms)')
+    if not directory_only:
+        res.append('$')
+    return ''.join(res)
--- tests.py
+++ tests.py
@@ -6,100 +6,100 @@ from unittest import TestCase
 
 
 class Test(TestCase):
-	def test_simple(self):
-		matches = _parse_gitignore_string(
-			'__pycache__/\n'
-			'*.py[cod]',
-			fake_base_dir='/home/michael'
-		)
-		self.assertFalse(matches('/home/michael/main.py'))
-		self.assertTrue(matches('/home/michael/main.pyc'))
-		self.assertTrue(matches('/home/michael/dir/main.pyc'))
-		self.assertTrue(matches('/home/michael/__pycache__'))
+    def test_simple(self):
+        matches = _parse_gitignore_string(
+            '__pycache__/\n'
+            '*.py[cod]',
+            fake_base_dir='/home/michael'
+        )
+        self.assertFalse(matches('/home/michael/main.py'))
+        self.assertTrue(matches('/home/michael/main.pyc'))
+        self.assertTrue(matches('/home/michael/dir/main.pyc'))
+        self.assertTrue(matches('/home/michael/__pycache__'))
 
-	def test_wildcard(self):
-		matches = _parse_gitignore_string(
-			'hello.*',
-			fake_base_dir='/home/michael'
-		)
-		self.assertTrue(matches('/home/michael/hello.txt'))
-		self.assertTrue(matches('/home/michael/hello.foobar/'))
-		self.assertTrue(matches('/home/michael/dir/hello.txt'))
-		self.assertTrue(matches('/home/michael/hello.'))
-		self.assertFalse(matches('/home/michael/hello'))
-		self.assertFalse(matches('/home/michael/helloX'))
+    def test_wildcard(self):
+        matches = _parse_gitignore_string(
+            'hello.*',
+            fake_base_dir='/home/michael'
+        )
+        self.assertTrue(matches('/home/michael/hello.txt'))
+        self.assertTrue(matches('/home/michael/hello.foobar/'))
+        self.assertTrue(matches('/home/michael/dir/hello.txt'))
+        self.assertTrue(matches('/home/michael/hello.'))
+        self.assertFalse(matches('/home/michael/hello'))
+        self.assertFalse(matches('/home/michael/helloX'))
 
-	def test_anchored_wildcard(self):
-		matches = _parse_gitignore_string(
-			'/hello.*',
-			fake_base_dir='/home/michael'
-		)
-		self.assertTrue(matches('/home/michael/hello.txt'))
-		self.assertTrue(matches('/home/michael/hello.c'))
-		self.assertFalse(matches('/home/michael/a/hello.java'))
+    def test_anchored_wildcard(self):
+        matches = _parse_gitignore_string(
+            '/hello.*',
+            fake_base_dir='/home/michael'
+        )
+        self.assertTrue(matches('/home/michael/hello.txt'))
+        self.assertTrue(matches('/home/michael/hello.c'))
+        self.assertFalse(matches('/home/michael/a/hello.java'))
 
-	def test_trailingspaces(self):
-		matches = _parse_gitignore_string(
-			'ignoretrailingspace \n'
-			'notignoredspace\\ \n'
-			'partiallyignoredspace\\  \n'
-			'partiallyignoredspace2 \\  \n'
-			'notignoredmultiplespace\\ \\ \\ ',
-			fake_base_dir='/home/michael'
-		)
-		self.assertTrue(matches('/home/michael/ignoretrailingspace'))
-		self.assertFalse(matches('/home/michael/ignoretrailingspace '))
-		self.assertTrue(matches('/home/michael/partiallyignoredspace '))
-		self.assertFalse(matches('/home/michael/partiallyignoredspace  '))
-		self.assertFalse(matches('/home/michael/partiallyignoredspace'))
-		self.assertTrue(matches('/home/michael/partiallyignoredspace2  '))
-		self.assertFalse(matches('/home/michael/partiallyignoredspace2   '))
-		self.assertFalse(matches('/home/michael/partiallyignoredspace2 '))
-		self.assertFalse(matches('/home/michael/partiallyignoredspace2'))
-		self.assertTrue(matches('/home/michael/notignoredspace '))
-		self.assertFalse(matches('/home/michael/notignoredspace'))
-		self.assertTrue(matches('/home/michael/notignoredmultiplespace   '))
-		self.assertFalse(matches('/home/michael/notignoredmultiplespace'))
+    def test_trailingspaces(self):
+        matches = _parse_gitignore_string(
+            'ignoretrailingspace \n'
+            'notignoredspace\\ \n'
+            'partiallyignoredspace\\  \n'
+            'partiallyignoredspace2 \\  \n'
+            'notignoredmultiplespace\\ \\ \\ ',
+            fake_base_dir='/home/michael'
+        )
+        self.assertTrue(matches('/home/michael/ignoretrailingspace'))
+        self.assertFalse(matches('/home/michael/ignoretrailingspace '))
+        self.assertTrue(matches('/home/michael/partiallyignoredspace '))
+        self.assertFalse(matches('/home/michael/partiallyignoredspace  '))
+        self.assertFalse(matches('/home/michael/partiallyignoredspace'))
+        self.assertTrue(matches('/home/michael/partiallyignoredspace2  '))
+        self.assertFalse(matches('/home/michael/partiallyignoredspace2   '))
+        self.assertFalse(matches('/home/michael/partiallyignoredspace2 '))
+        self.assertFalse(matches('/home/michael/partiallyignoredspace2'))
+        self.assertTrue(matches('/home/michael/notignoredspace '))
+        self.assertFalse(matches('/home/michael/notignoredspace'))
+        self.assertTrue(matches('/home/michael/notignoredmultiplespace   '))
+        self.assertFalse(matches('/home/michael/notignoredmultiplespace'))
 
-	def test_comment(self):
-		matches = _parse_gitignore_string(
+    def test_comment(self):
+        matches = _parse_gitignore_string(
                         'somematch\n'
                         '#realcomment\n'
                         'othermatch\n'
                         '\\#imnocomment',
-			fake_base_dir='/home/michael'
-		)
-		self.assertTrue(matches('/home/michael/somematch'))
-		self.assertFalse(matches('/home/michael/#realcomment'))
-		self.assertTrue(matches('/home/michael/othermatch'))
-		self.assertTrue(matches('/home/michael/#imnocomment'))
+            fake_base_dir='/home/michael'
+        )
+        self.assertTrue(matches('/home/michael/somematch'))
+        self.assertFalse(matches('/home/michael/#realcomment'))
+        self.assertTrue(matches('/home/michael/othermatch'))
+        self.assertTrue(matches('/home/michael/#imnocomment'))
 
-	def test_ignore_directory(self):
-		matches = _parse_gitignore_string('.venv/', fake_base_dir='/home/michael')
-		self.assertTrue(matches('/home/michael/.venv'))
-		self.assertTrue(matches('/home/michael/.venv/folder'))
-		self.assertTrue(matches('/home/michael/.venv/file.txt'))
+    def test_ignore_directory(self):
+        matches = _parse_gitignore_string('.venv/', fake_base_dir='/home/michael')
+        self.assertTrue(matches('/home/michael/.venv'))
+        self.assertTrue(matches('/home/michael/.venv/folder'))
+        self.assertTrue(matches('/home/michael/.venv/file.txt'))
 
-	def test_ignore_directory_astrix(self):
-		matches = _parse_gitignore_string('.venv/*', fake_base_dir='/home/michael')
-		self.assertFalse(matches('/home/michael/.venv'))
-		self.assertTrue(matches('/home/michael/.venv/folder'))
-		self.assertTrue(matches('/home/michael/.venv/file.txt'))
+    def test_ignore_directory_astrix(self):
+        matches = _parse_gitignore_string('.venv/*', fake_base_dir='/home/michael')
+        self.assertFalse(matches('/home/michael/.venv'))
+        self.assertTrue(matches('/home/michael/.venv/folder'))
+        self.assertTrue(matches('/home/michael/.venv/file.txt'))
 
-	def test_negation(self):
-		matches = _parse_gitignore_string(
-			'''
+    def test_negation(self):
+        matches = _parse_gitignore_string(
+            '''
 *.ignore
 !keep.ignore
-			''',
-			fake_base_dir='/home/michael'
-		)
-		self.assertTrue(matches('/home/michael/trash.ignore'))
-		self.assertFalse(matches('/home/michael/keep.ignore'))
-		self.assertTrue(matches('/home/michael/waste.ignore'))
+            ''',
+            fake_base_dir='/home/michael'
+        )
+        self.assertTrue(matches('/home/michael/trash.ignore'))
+        self.assertFalse(matches('/home/michael/keep.ignore'))
+        self.assertTrue(matches('/home/michael/waste.ignore'))
 
 
 def _parse_gitignore_string(data: str, fake_base_dir: str = None):
-	with patch('builtins.open', mock_open(read_data=data)):
-		success = parse_gitignore(f'{fake_base_dir}/.gitignore', fake_base_dir)
-		return success
+    with patch('builtins.open', mock_open(read_data=data)):
+        success = parse_gitignore(f'{fake_base_dir}/.gitignore', fake_base_dir)
+        return success
