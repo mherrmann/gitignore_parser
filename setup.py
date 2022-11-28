59605c5a36db48e18730886459945cfe073f7e3e
Michael Herrmann <michael@herrmann.io>
2022-11-28 14:12:50 +0100

    Set version number for release 0.1.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 description = 'A spec-compliant gitignore parser for Python 3.5+'
 setup(
     name='gitignore_parser',
-    version='0.1.0',
+    version='0.1.2',
     install_requires=["pathlib; python_version < '3.0'"],
     description=description,
     long_description=
