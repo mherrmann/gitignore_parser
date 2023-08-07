8554b269937174e61e93ae572dbde502d865e4bb
Ruan Comelli <ruancomelli@gmail.com>
2023-08-07 03:48:23 -0300

    List Python versions 3.8 to 3.11 in the classifiers (#50)

--- setup.py
+++ setup.py
@@ -30,6 +30,10 @@ setup(
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
 
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules'
