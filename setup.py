053027784f578b9c71011c7e061e4660ea2ba579
Michael Herrmann <michael@herrmann.io>
2025-08-25 08:32:42 +0200

    Set version number for release 0.1.13

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 description = 'A spec-compliant gitignore parser for Python 3.5+'
 setup(
     name='gitignore_parser',
-    version='0.1.12',
+    version='0.1.13',
     description=description,
     long_description=
         description + '\n\nhttps://github.com/mherrmann/gitignore_parser',
